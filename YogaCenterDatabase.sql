e18f452317aa3eb4436c3c496064af3d1cfb0a7a
Minh Nguyên <108600635+Nguyennm080503@users.noreply.github.com>
2023-05-23 09:01:49 +0700

    Add files via upload

---
+++ YogaCenterDatabase.sql
@@ -0,0 +1,93 @@
+CREATE DATABASE YogaCenter
+
+CREATE TABLE Accounts(
+IDaccount int IDENTITY(1,1) PRIMARY KEY,
+Email varchar(50) UNIQUE,
+Name Nvarchar(50) NOT NULL,
+Phone varchar(10) NOT NULL,
+Address Nvarchar(100),
+)
+
+CREATE TABLE Employee(
+ID_Employee int IDENTITY(1,1) PRIMARY KEY,
+ID_Account int FOREIGN KEY REFERENCES Accounts(IDaccount),
+Account varchar(50),
+Password varchar(50),
+Role int check(Role = 0 or Role = 1 or Role = 2), --admin:0, staff:1, trainer:2
+Status int check(Status = 0 or Status = 1) --active:0,unactive:1
+)
+
+CREATE TABLE Trainee(
+ID_Trainee int IDENTITY(1,1) PRIMARY KEY,
+ID_Account int FOREIGN KEY REFERENCES Accounts(IDaccount),
+Status int CHECK(Status = 0 or Status = 1) --active:0,unactive:1
+)
+
+CREATE TABLE Level(
+Level_ID int IDENTITY(1,1) PRIMARY KEY,
+Level_Name Nvarchar(50)
+)
+
+CREATE TABLE Course(
+Course_ID int IDENTITY(1,1) PRIMARY KEY,
+Course_Name Nvarchar(100) NOT NULL,
+Course_Fee DECIMAL(18,2), 
+Start_date date,
+End_date date,
+Description Nvarchar(MAX),
+ID_Level int FOREIGN KEY REFERENCES Level(Level_ID),
+ID_Trainer int FOREIGN KEY REFERENCES Employee(ID_Employee),
+Status int CHECK(Status = 0 or Status = 1) --active:1,unactive:0
+)
+
+CREATE TABLE Class(
+Class_ID int IDENTITY(1,1) PRIMARY KEY,
+Class_Name varchar(10),
+Status int CHECK(Status = 0 or Status = 1)
+)
+
+CREATE TABLE Time(
+Time_ID int IDENTITY(1,1) PRIMARY KEY,
+Time_Choose Nvarchar(50)
+)
+
+CREATE TABLE Date(
+Date_ID int IDENTITY(1,1) PRIMARY KEY,
+Date_Name Nvarchar(50)
+)
+
+CREATE TABLE ClassDetail(
+ClassDetail_ID int IDENTITY(1,1) PRIMARY KEY,
+Class_ID int FOREIGN KEY REFERENCES Class(Class_ID),
+IDtime int FOREIGN KEY REFERENCES Time(Time_ID),
+IDDate int FOREIGN KEY REFERENCES Date(Date_ID),
+IDCourse int FOREIGN KEY REFERENCES Course(Course_ID),
+)
+
+CREATE TABLE BookingCourse(
+OrderID int IDENTITY(1,1) PRIMARY KEY,
+ID_Trainee int FOREIGN KEY REFERENCES Trainee(ID_Trainee),
+DateOrder DATE,
+Method int CHECK(Method = 0 or Method = 1) --0:cash,1:onlinebanking
+)
+
+CREATE TABLE StatusPayment(
+ID_Payment int IDENTITY(1,1) PRIMARY KEY,
+ID_Order int FOREIGN KEY REFERENCES BookingCourse(OrderID),
+Status int check(Status = 1 or Status = 0 or Status = 2) --0:not yet,1:complete,2:cancel
+)
+
+CREATE TABLE BookingDetail(
+BookingDetail_ID int IDENTITY(1,1) PRIMARY KEY,
+Order_ID int FOREIGN KEY REFERENCES BookingCourse(OrderID),
+ID_Course int FOREIGN KEY REFERENCES Course(Course_ID),
+Quantity int NOT NULL
+)
+
+CREATE TABLE CheckAttendance(
+Attendance_ID int IDENTITY(1,1) PRIMARY KEY,
+ID_Trainee int FOREIGN KEY REFERENCES Trainee(ID_Trainee),
+ID_Course int FOREIGN KEY REFERENCES Course(Course_ID),
+AttendanceDate date,
+Status int check(Status = 0 or Status = 1) --0:absent,1:present
+)
